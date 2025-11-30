e65af0e990a4490e07f71932520f40281dd558cd
Martina Delger <martidelger@gmail.com>
2025-11-30 02:00:17 +0000

    minor edits to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 #include "CampusCompass.h"
+using namespace std;
 
 int main() {
     CampusCompass compass;
     compass.parse_csv("data/edges.csv", "data/classes.csv");
     int n;
-    if (!(std::cin >> n)) return 0;
-    std::string line;
-    std::getline(std::cin, line); 
+    if (!(cin >> n)) return 0;
+    string line;
+    getline(cin, line); 
     for (int i = 0; i < n; ++i) {
-        if (!std::getline(std::cin, line)) break;
+        if (!getline(cin, line)) break;
         compass.parse_command(line);
     }
     return 0;
